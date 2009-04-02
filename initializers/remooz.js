1499a8bae5061f7de53c6ac516fbda8a4ac9593a
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-04-02 13:26:58 +0200

    Modified initializer, it should works now

--- initializers/remooz.js
+++ initializers/remooz.js
@@ -1,17 +1,15 @@
-window.addEvent('domready', {
-  /**
-   * Assigns ReMooz behavior to all anchors with the .remooz class
-   */
-  ReMooz.assign();
+// yep, it really requires load event, don't know why
+window.addEvent('load', function() {
+  // Assigns ReMooz behavior to all anchors with the .remooz class
+  // ReMooz.assign();
  
-  /**
-   * Or assign ReMooz "by hand"
-   */
-  //$$('ul.thumbnails a').each(function(element) {
-  //  // Constructor, takes the element and options as arguments
-  //  new ReMooz(element, {
-  //    centered: true, // Zoom the center of the screen
-  //    origin: element.getElement('img') // Take the image inside as origin for the zooming element
-  //  });
-  //});
+  // Or assign ReMooz "by hand"
+  $$('.remooz').each(function(element) {
+    alert(element)
+   // Constructor, takes the element and options as arguments
+   new ReMooz(element, {
+     centered: true, // Zoom the center of the screen
+     origin: element.getElement('img') // Take the image inside as origin for the zooming element
+   });
+  });
 });
